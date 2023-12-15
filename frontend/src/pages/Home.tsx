66b1291299d5a2e8ffb737eb47d02f8675151458
Avner José <avnerj.g.r@gmail.com>
2023-12-15 02:56:09 -0300

    feat: create new contact dialog

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,17 @@
 import { PhoneBookItem } from "@/components/PhoneBookItem";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { PhoneBookEntry } from "@/entities/PhoneBookEntry";
 import { api } from "@/services/api";
 import { Search } from "lucide-react";
@@ -25,9 +37,43 @@ export function HomePage() {
         <div>
           <div className="flex justify-between items-center">
             <h1 className="text-dark-700 text-4xl font-semibold">Contacts</h1>
-            <Button className="bg-blue-500 text-white text-lg p-6">
-              + Add Contact
-            </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="bg-blue-500 text-white text-lg p-6">
+                  + Add Contact
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                  <DialogTitle>Add new contact</DialogTitle>
+                  <DialogDescription>
+                    Fill out the following fiealds to add a new contact
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="firstName">First name</Label>
+                  <Input id="firstName" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="lastName">Last name</Label>
+                  <Input id="lastName" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="phone">Phone number</Label>
+                  <Input id="phone" />
+                </div>
+                <DialogFooter className="sm:justify-start">
+                  <DialogClose asChild>
+                    <Button
+                      type="button"
+                      className="border border-blue-500 text-blue-500 bg-blue-50"
+                    >
+                      Create new contact
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </div>
           <div className="flex gap-1 items-center w-full mt-6 border-2 rounded-md focus-within:outline focus-within:outline-1 border-gray-500 px-2 bg-white">
             <Search />
