ceb83d16a8d1102c2322d7664fea9ebd6a52b84d
Avner José <avnerj.g.r@gmail.com>
2023-12-15 05:05:04 -0300

    docs: implementing swagger documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import express, { json } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 import cors from "cors";
 
 import { phoneBookEntriesRouter } from "./routes/phone-book-entries.routes";
@@ -8,6 +10,8 @@ const app = express();
 app.use(json());
 
 app.use(cors());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/phone-book", phoneBookEntriesRouter);
 
