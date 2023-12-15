ceeb5bbab51b54e016d73e9df205e180e356e7e6
Avner José <avnerj.g.r@gmail.com>
2023-12-15 02:28:30 -0300

    feat: creating phone entry display

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
+import { Pencil, Phone, Search, Trash2 } from "lucide-react";
 
 export function HomePage() {
   return (
@@ -18,6 +18,27 @@ export function HomePage() {
               className="w-full py-2 outline-none"
               placeholder="Search for contact by last name..."
             />
+          </div>
+        </div>
+        <div className="mt-8">
+          <div className="bg-white flex justify-between items-center py-6 px-4 first:rounded-t-md border-2 border-gray-500">
+            <div>
+              <h4 className="text-dark-700 text-3xl font-semibold">
+                Eric Elliot
+              </h4>
+              <span className="flex gap-1 items-center text-xl font-bold text-gray-500">
+                <Phone size={18} />
+                222-555-6576
+              </span>
+            </div>
+            <div className="flex gap-1">
+              <Button className="bg-green-500 p-3 w-12 h-12">
+                <Pencil className="text-white" />
+              </Button>
+              <Button className="bg-red-500 p-3 w-12 h-12">
+                <Trash2 className="text-white" />
+              </Button>
+            </div>
           </div>
         </div>
       </main>
