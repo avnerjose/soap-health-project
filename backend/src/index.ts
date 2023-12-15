2719bdcfa5b44799b9c4ecd007847d8aadac5fe8
Avner José <avnerj.g.r@gmail.com>
2023-12-15 02:41:43 -0300

    fix: fixing cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express, { json } from "express";
+import cors from "cors";
+
 import { phoneBookEntriesRouter } from "./routes/phone-book-entries.routes";
 
 const app = express();
 
 app.use(json());
+
+app.use(cors());
 
 app.use("/phone-book", phoneBookEntriesRouter);
 
